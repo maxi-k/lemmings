8369d9af6825926f11d4cf365e726889c2ec5299
Maxi <maxi.kuschewski@gmail.com>
2015-12-17 15:14:20 +0100

    Add object-orientation

--- src/core.rb
+++ src/core.rb
@@ -1,2 +1,17 @@
-puts "Hello World!"
-puts "I hate everyone!"
+class Greeter
+  def initialize(msg)
+    @message = msg
+  end
+
+  def get_message(subject)
+    @message % subject
+  end
+end
+
+hello = Greeter.new("Hello, %s!")
+puts hello.get_message "World"
+puts hello.get_message "Everyone"
+puts
+bye = Greeter.new "Until next time, %s!"
+puts bye.get_message "World"
+puts bye.get_message "Everyone"
